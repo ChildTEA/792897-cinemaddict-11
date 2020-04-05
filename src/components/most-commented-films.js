8ce2128b81b08437e3a1e9d14f501c964ab3a946
Yaroslav Smirnov <yar14@mail.ru>
2020-04-05 18:35:30 +0300

    refactor (components rendering): Код отрисовки компонентов разделён на модули

---
+++ src/components/most-commented-films.js
@@ -0,0 +1,10 @@
+const createMostCommentedFilmsTemplate = () => {
+  return (
+    `<section class="films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+      <div class="films-list__container" id="most-commented-films-list"></div>
+    </section>`
+  );
+};
+
+export {createMostCommentedFilmsTemplate};
