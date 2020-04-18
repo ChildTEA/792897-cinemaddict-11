84346a51dcf15d7734a5a1a0f8123c398dcd1f0e
Yaroslav Smirnov <yar14@mail.ru>
2020-04-18 20:53:28 +0300

    feat (index rendering): Реализована отрисовка карточек на основе сгенерированных данных

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+import {getRandomArrayItems} from './util.js';
+import {generateFilms} from './mock/films.js';
+
 import {createFilmCardTemplate} from './components/film-card.js';
-import {createFilmDetailsPopupTemplate} from './components/film-details-popup.js';
+// import {createFilmDetailsPopupTemplate} from './components/film-details-popup.js';
 import {createFilmsListContainerTemplate} from './components/films-list.js';
 import {createFilmsSectionTemplate} from './components/films-section.js';
 import {createHeaderProfileTemplate} from './components/header-profile.js';
@@ -8,25 +11,34 @@ import {createMostCommentedFilmsTemplate} from './components/most-commented-film
 import {createShowMoreButtonTemplate} from './components/show-more-button.js';
 import {createTopRatedFilmsTemplate} from './components/top-rated-films.js';
 
-const FILMS_COUNT = 5;
+const FILMS_COUNT = 20;
+const GENERAL_FILMS_COUNT = 5;
 const TOP_RATED_FILMS_COUNT = 2;
 const MOST_COMMENTED_FILMS_COUNT = 2;
 
-const siteBodyElement = document.querySelector(`body`);
+// const siteBodyElement = document.querySelector(`body`);
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 
+const films = generateFilms(FILMS_COUNT);
+
+const generalFilms = getRandomArrayItems(films, GENERAL_FILMS_COUNT);
+
+const topRatedFilms = films
+  .sort((a, b) => {
+    return b.rating - a.rating;
+  })
+  .slice(0, TOP_RATED_FILMS_COUNT);
+
+const mostCommentedFilms = films
+  .sort((a, b) => {
+    return b.comments.length - a.comments.length;
+  })
+  .slice(0, MOST_COMMENTED_FILMS_COUNT);
 
 const renderComponent = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
-
-const renderComponentNTimes = (container, template, count = 1, place = `beforeend`) => {
-  for (let i = 0; i < count; i++) {
-    container.insertAdjacentHTML(place, template);
-  }
-};
-
 
 // Site header
 renderComponent(siteHeaderElement, createHeaderProfileTemplate());
@@ -48,7 +60,9 @@ renderComponent(filmsElement, createFilmsListContainerTemplate());
 const filmsListElement = filmsElement.querySelector(`.films-list`);
 const filmsListContainerElement = filmsListElement.querySelector(`.films-list__container`);
 
-renderComponentNTimes(filmsListContainerElement, createFilmCardTemplate(), FILMS_COUNT);
+generalFilms.forEach((it) => {
+  renderComponent(filmsListContainerElement, createFilmCardTemplate(it));
+});
 renderComponent(filmsListElement, createShowMoreButtonTemplate());
 
 
@@ -57,15 +71,18 @@ renderComponent(filmsElement, createTopRatedFilmsTemplate());
 
 const topRatedFilmsListElement = filmsElement.querySelector(`#top-rated-films-list`);
 
-renderComponentNTimes(topRatedFilmsListElement, createFilmCardTemplate(), TOP_RATED_FILMS_COUNT);
+topRatedFilms.forEach((it) => {
+  renderComponent(topRatedFilmsListElement, createFilmCardTemplate(it));
+});
 
-
-//  Most commented films
 renderComponent(filmsElement, createMostCommentedFilmsTemplate());
 
 const mostCommentedFilmsListElement = filmsElement.querySelector(`#most-commented-films-list`);
 
-renderComponentNTimes(mostCommentedFilmsListElement, createFilmCardTemplate(), MOST_COMMENTED_FILMS_COUNT);
+mostCommentedFilms.forEach((it) => {
+  renderComponent(mostCommentedFilmsListElement, createFilmCardTemplate(it));
+});
+
 
 // Popup
-renderComponent(siteBodyElement, createFilmDetailsPopupTemplate());
+// renderComponent(siteBodyElement, createFilmDetailsPopupTemplate());
