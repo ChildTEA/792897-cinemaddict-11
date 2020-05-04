3b6bdcf351d6b0964438bdf1bcc364ef09c4f68f
Yaroslav Smirnov <yar14@mail.ru>
2020-05-04 20:38:17 +0300

    feat: Реализована сортировка фильмов по дате и рейтингу.

---
+++ src/components/sort.js
@@ -0,0 +1,53 @@
+import AbstractComponent from '../components/abstract-component.js';
+
+export const SortType = {
+  DATE: `date-down`,
+  RATING: `rating-down`,
+  DEFAULT: `default`,
+};
+
+const createSortTemplate = (currentSortType) => {
+
+  return (
+    `<ul class="sort">
+      <li><a href="#" class="sort__button${currentSortType === SortType.DEFAULT ? ` sort__button--active` : ``}" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+      <li><a href="#" class="sort__button${currentSortType === SortType.DATE ? ` sort__button--active` : ``}" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+      <li><a href="#" class="sort__button${currentSortType === SortType.RATING ? ` sort__button--active` : ``}" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
+    </ul>`
+  );
+};
+
+export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+    this._currentSortType = SortType.DEFAULT;
+  }
+
+  getTemplate() {
+    return createSortTemplate(this._currentSortType);
+  }
+
+  getSortType() {
+    return this._currentSortType;
+  }
+
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      if (evt.target.tagName !== `A`) {
+        return;
+      }
+
+      const sortType = evt.target.dataset.sortType;
+
+      if (sortType === this._currentSortType) {
+        return;
+      }
+
+      this._currentSortType = sortType;
+
+      handler(this._currentSortType);
+    });
+  }
+}
